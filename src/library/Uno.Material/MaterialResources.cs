48936bae0060f905a26abf2dd7b7e796c9aaa553
Eiad Al-Khanshour <eiadxp@hotmail.com>
2021-06-03 12:29:31 +0300

    Try to use XAML resources instead of C# initializing. This should solve the XAML designer problems

--- src/library/Uno.Material/MaterialResources.cs
+++ src/library/Uno.Material/MaterialResources.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 #if WinUI
 using Microsoft.UI.Xaml;
@@ -13,7 +14,21 @@ namespace Uno.Material
 	/// </summary>
 	public sealed class MaterialResources : ResourceDictionary
 	{
-		public MaterialResources()
+		public MaterialResources() => this.InitializeResources();
+		//public MaterialResources(Uri colorPaletteSource) => this.ColorPaletteSource = colorPaletteSource;
+
+		private Uri _ColorPaletteSource;
+		public Uri ColorPaletteSource
+		{
+			get => _ColorPaletteSource;
+			set
+			{
+				_ColorPaletteSource = value;
+				SetColorPaletteSource();
+			}
+		}
+
+		private void InitializeResources()
 		{
 			// Add all ResourceDictionaries for Variables here in alphabetical order
 			this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Uno.Material/Styles/Application/AnimationConstants.xaml") });
@@ -56,6 +71,59 @@ namespace Uno.Material
 			this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Uno.Material/Styles/Controls/TimePicker.xaml") });
 			this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Uno.Material/Styles/Controls/ToggleButton.xaml") });
 			this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Uno.Material/Styles/Controls/ToggleSwitch.xaml") });
+		}
+		private void SetColorPaletteSource()
+		{
+			if (ColorPaletteSource == null)
+			{
+				foreach (var dictionary in GetColorResourceDictionaries())
+				{
+					while (dictionary.MergedDictionaries.Count > 1)
+					{
+						dictionary.MergedDictionaries.RemoveAt(dictionary.MergedDictionaries.Count - 1);
+					}
+				}
+			}
+			else
+			{
+				foreach (var dictionary in GetColorResourceDictionaries())
+				{
+					if (dictionary.MergedDictionaries.Count == 1)
+					{
+						dictionary.MergedDictionaries.Add(new ResourceDictionary() { Source = ColorPaletteSource });
+					}
+					else
+					{
+						dictionary.MergedDictionaries[0] = new ResourceDictionary() { Source = ColorPaletteSource };
+					}
+				}
+			}
+		}
+
+		private List<ResourceDictionary> colorResourceDictionaries;
+		private List<ResourceDictionary> GetColorResourceDictionaries()
+		{
+			if (colorResourceDictionaries == null)
+			{
+				colorResourceDictionaries = new List<ResourceDictionary>();
+				foreach (var dictionary in MergedDictionaries)
+				{
+					GetColorResourceDictionaries(dictionary);
+				}
+			}
+			return colorResourceDictionaries;
+		}
+		private void GetColorResourceDictionaries(ResourceDictionary dictionary)
+		{
+			colorResourceDictionaries = colorResourceDictionaries ?? new List<ResourceDictionary>();
+			if (dictionary.Source != null && dictionary.Source.AbsoluteUri.EndsWith("Colors.xaml"))
+			{
+				colorResourceDictionaries.Add(dictionary);
+			}
+			foreach (var item in dictionary.MergedDictionaries)
+			{
+				GetColorResourceDictionaries(item);
+			}
 		}
 	}
 }
